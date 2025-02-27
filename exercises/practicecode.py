1aa74ce6d277cd9045b867a6895a72a7d342693d
dika <nsamah@unc.edu>
2025-02-27 00:49:56 -0500

    Exercise 02

---
+++ exercises/practicecode.py
@@ -0,0 +1,13 @@
+def guess_secret(word: str, secret: str, idx: int = 0) -> bool:
+    
+    if len(word) != len(secret):
+        print("Words are different lengths.")
+        return False
+
+    if word[idx] != secret[idx]:
+        print (f"{word[idx]}) isn't the secret word's next letter.")
+        return False
+    else   
+
+
+
